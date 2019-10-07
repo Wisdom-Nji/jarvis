1bc3b017e5009b12e516aaaf86a49ffa70ebc2da
sherlock wisdom <wisdomnji@gmail.com>
2019-10-07 21:49:37 +0100

    not feeling too good rn...

--- main.py
+++ main.py
@@ -4,5 +4,32 @@
 import csv;
 
 
+class Node:
+
+	def __init__(self, csv_data ):
+		'convert from DictReader to List'
+		headers = csv_data.fieldnames
+
+		self.dataset = {}
+		for columns in csv_data:
+			for header in headers:
+				row = columns[header]
+				print("[%s]" %(header))
+				print(row)
+				
+		#print(self.dataset)
 
 
+
+def read_csv_file( filename ):
+	csv_file = open(filename);
+	csv_data = csv.DictReader( csv_file );
+
+	#print(csv_data.fieldnames
+	return csv_data
+
+
+csv_data = read_csv_file( "dataset/Users.csv" )
+
+
+node = Node( csv_data )
